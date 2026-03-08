4ea7401887b77253a8bd67a2b77fc544ca5fe931
freeworld <>
2026-03-08 22:06:19 +0100

    Navbar fix small laptop size

--- navbar.js
+++ navbar.js
@@ -2,14 +2,19 @@ const checkbox = document.getElementById('nav-toggle');
 const navMobile = document.querySelector('.nav-mobile');
 
 checkbox.addEventListener('change', () => {
-    const visible = navMobile.getAttribute('data-visible') === 'true';
-    if (!visible) {
-        navMobile.style.animation = "slide-in 0.5s";
-        navMobile.setAttribute('data-visible', 'true');
-        console.log('Menu opened');
-    } else {
-        navMobile.style.animation = "slide-down 0.5s";
+    if (checkbox.checked) {
+        navMobile.style.animation = "slide-in 0.3s";
         navMobile.setAttribute('data-visible', 'false');
+        console.log('Menu opened');
+    }  
+});
+
+checkbox.addEventListener('change', () => {
+    if (!checkbox.checked) {
+         navMobile.style.animation = "slide-down 0.3s ";
+        navMobile.setAttribute('data-visible', 'true');
         console.log('Menu closed');
+    }  else{
+      
     }
 });
