15bcd534d99ddec172cf428ad8be1fefcd2f2f7b
freeworld <>
2026-08-25 04:17:02 +0200

    process email add

--- pages/question_process.php
+++ pages/question_process.php
@@ -1,33 +1,34 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // 1. Instellingen (PAS DIT AAN)
-    $ontvanger = "r.p.v.marten@gmail.com"; // Het adres waar de mail binnenkomt
+    $ontvanger = "redanincforce@redalcos.com"; // Het adres waar de mail binnenkomt
     $afzender_strato = "redanincforce@redalcos.com"; // MUST: Bestaand Strato e-mailadres!
     
     // HIER DE LINK NAAR UW BEDANKPAGINA INVULLEN:
     $bedank_pagina = "" ;
 
     // 2. Input filteren tegen spam/injecties
-    $klant_naam = filter_var(strip_tags(trim($_POST['naam'])), FILTER_SANITIZE_SPECIAL_CHARS);
+    $fname = filter_var(strip_tags(trim($_POST['fname'])), FILTER_SANITIZE_SPECIAL_CHARS);
+    $lname = filter_var(strip_tags(trim($_POST['lname'])), FILTER_SANITIZE_SPECIAL_CHARS);
     $klant_email = filter_var(trim($_POST['email']), FILTER_VALIDATE_EMAIL);
-    $klant_bericht = filter_var(strip_tags(trim($_POST['bericht'])), FILTER_SANITIZE_SPECIAL_CHARS);
+    $klant_bericht = filter_var(strip_tags(trim($_POST['question'])), FILTER_SANITIZE_SPECIAL_CHARS);
 
-    if (!$klant_email || empty($klant_naam) || empty($klant_bericht)) {
+    if (!$klant_email || empty($fname) || empty($lname) || empty($klant_bericht)) {
         echo "Vul alstublieft alle velden correct in.";
         exit;
     }
 
     // 3. E-mail opbouwen
-    $onderwerp = "Nieuw bericht van " . $klant_naam;
+    $onderwerp = "Nieuw bericht van " . $fname . " " . $lname;
     
     $inhoud = "Er is een nieuw bericht ontvangen via de website:\n\n";
-    $inhoud .= "Naam: " . $klant_naam . "\n";
+    $inhoud .= "Naam: " . $fname . " " . $lname . "\n";
     $inhoud .= "E-mail: " . $klant_email . "\n\n";
     $inhoud .= "Bericht:\n" . $klant_bericht . "\n";
 
     // 4. Strato-conforme e-mailheaders
     $headers = "From: Website Formulier <" . $afzender_strato . ">\r\n";
-    $headers .= "Reply-To: " . $klant_naam . " <" . $klant_email . ">\r\n";
+    $headers .= "Reply-To: " . $fname . " " . $lname . " <" . $klant_email . ">\r\n";
     $headers .= "X-Mailer: PHP/" . phpversion() . "\r\n";
     $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
